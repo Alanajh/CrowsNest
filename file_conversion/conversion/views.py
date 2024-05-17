241a21f383bda17841488aed064232d973737bed
Alana Harmon <alana.harmon@yahoo.com>
2024-05-17 00:51:34 -0500

    Connect db to filter user accounts for login validations

--- file_conversion/conversion/views.py
+++ file_conversion/conversion/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 
 def logins(request):
+    #template = loader.get_template('index.html')
+    #return HttpResponse(template.render())
+    accounts = User.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'userAccounts': accounts,
+    }
+    return HttpResponse(template.render(context, request))
