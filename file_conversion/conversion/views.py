1f756e0d2581c16b08eac3b5efd3b8c22ed4e037
Alana Harmon <alana.harmon@yahoo.com>
2024-05-19 01:15:38 -0500

    Wrap username and password in form

--- file_conversion/conversion/views.py
+++ file_conversion/conversion/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import User
 
 def logins(request):
     #template = loader.get_template('index.html')
     #return HttpResponse(template.render())
     accounts = User.objects.all()
+    accts = User.objects.filter(username="test").values()
     template = loader.get_template('index.html')
     context = {
-        'userAccounts': accounts,
+        'userAccounts': accts,
     }
     return HttpResponse(template.render(context, request))
+
+def valid_user(request):
+    
+    return render(request, 'conversion/index.html', {})
